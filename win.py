209e45168aec088e38d39b011dd05b954bf09fcc
Imtiyaz Akiwat <94955223+imtiyazakiwat@users.noreply.github.com>
2024-01-16 21:32:04 +0530

    priority correction

--- win.py
+++ win.py
@@ -1,6 +1,7 @@
 def decide_winner(cards, hukum_suit, starting_suit):
     # Define the priority order for the cards
-    card_priority = {'7': 0, '8': 1, '9': 2, '10': 3, 'Jack': 4, 'Queen': 5, 'King': 6, 'Ace': 7}
+    card_priority = {'7': 0, '8': 1, '9': 2, '10': 3, 'Jack': 4, 'Queen': 5, 'King': 6, 'Ace': 7 , }
+    hukum_suit_priority = {}
 
     # Sort the cards based on the priority order
     sorted_cards = sorted(cards, key=lambda x: (hukum_suit != x[0], starting_suit != x[0], card_priority[x[1]]))
